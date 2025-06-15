393d4d4cc051c45803da7004f9f2886bcf2edf50
Mohan-raj05 <mohanraj@gramtrix.in>
2025-06-15 13:20:36 +0530

    I have practiced and implemented the conditional and looping statements in Java

---
+++ C2TC_CoreJavaPrograms/src/Day2/ForLoopExample.java
@@ -0,0 +1,13 @@
+package Day2;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		for (int i =1; i<=1000;i++) {
+			System.out.println("value of i :" + i);
+			
+		}
+
+	}
+
+}
---
+++ C2TC_CoreJavaPrograms/src/Day2/WhileExampleProgram.java
@@ -0,0 +1,15 @@
+package Day2;
+
+public class WhileExampleProgram {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
